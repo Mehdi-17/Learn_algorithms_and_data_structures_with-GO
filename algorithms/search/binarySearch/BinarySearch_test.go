3d21004b764304be38769a4a0ac874eaabbc4d3a
Mehdi Serra <mehdi.serra57@gmail.com>
2023-10-01 10:44:59 +0200

    Add some test cases for BinarySearch_test.go && linearSearch_test.go

--- algorithms/search/binarySearch/BinarySearch_test.go
+++ algorithms/search/binarySearch/BinarySearch_test.go
@@ -15,6 +15,14 @@ func TestBinarySearch(t *testing.T) {
 	testCases := []testCase{
 		{"ElementExists", []int{1, 2, 4, 5, 7, 12, 14, 15, 17, 27, 37, 117, 1717}, 17, 8},
 		{"ElementExists", []int{1, 5, 7, 12, 18, 27, 37, 117, 1717}, 17, -1},
+		{"MiddleInSortedArray", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5, 4},
+		{"FirstElement", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 1, 0},
+		{"LastElement", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 10, 9},
+		{"EvenNumbers", []int{2, 4, 6, 8, 10, 12, 14, 16}, 8, 3},
+		{"MultiplesOf3", []int{3, 6, 9, 12, 15, 18, 21}, 18, 5},
+		{"ElementBiggerThanWholeArray", []int{1, 2, 3, 4, 5}, 6, -1},
+		{"ElementSmallerThanWholeArray", []int{2, 4, 6, 8, 10}, 1, -1},
+		{"EmptyArray", []int{}, 5, -1},
 	}
 
 	for _, tc := range testCases {
--- algorithms/search/linearSearch/linearSearch_test.go
+++ algorithms/search/linearSearch/linearSearch_test.go
@@ -13,6 +13,14 @@ func TestLinearSearch(t *testing.T) {
 	testCases := []testCase{
 		{"ElementExists", []int{15, 6, 17, 56, 561, 45}, 17, 2},
 		{"ElementNoExists", []int{15, 6, 17, 56, 561, 45}, 196, -1},
+		{"ElementExistsMiddle", []int{15, 6, 17, 56, 561, 45}, 17, 2},
+		{"ElementExistsBeginning", []int{15, 6, 17, 56, 561, 45}, 15, 0},
+		{"ElementExistsEnd", []int{15, 6, 17, 56, 561, 45}, 45, 5},
+		{"ElementExistsSmallArray", []int{1, 2, 3, 4, 5}, 5, 4},
+		{"ElementExistsSmallArray", []int{1, 2, 3, 4, 5}, 2, 1},
+		{"ElementNotExistsSmallArray", []int{1, 2, 3, 4, 5}, 6, -1},
+		{"ElementNotExistsSingleElementArray", []int{1}, 2, -1},
+		{"EmptyArray", []int{}, 5, -1},
 	}
 
 	for _, tc := range testCases {
