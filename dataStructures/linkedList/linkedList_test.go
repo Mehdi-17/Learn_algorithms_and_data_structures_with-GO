7818e851d788b7e4b1ccd1114940d82c85a5f15a
Mehdi Serra <mehdi.serra57@gmail.com>
2023-10-20 09:47:54 +0200

    Refacto linkedList_test.go

--- dataStructures/linkedList/linkedList_test.go
+++ dataStructures/linkedList/linkedList_test.go
@@ -2,13 +2,16 @@ package linkedList
 
 import "testing"
 
-func TestLinkedList(t *testing.T) {
+func TestCreateNode(t *testing.T) {
 	//Test createNode function
 	head := createNode('A')
 	if head.value != 'A' {
 		t.Errorf("error in createNode, want : 'A', got : %c", head.value)
 	}
+}
 
+func TestAppendNode(t *testing.T) {
+	head := createNode('A')
 	//Test appendNode function
 	appendedNode, _ := appendNode(head, 'B')
 	if appendedNode.value != 'B' {
@@ -17,7 +20,10 @@ func TestLinkedList(t *testing.T) {
 	if appendedNode.prevVal != head {
 		t.Errorf("error in appended node previous value, want %c, got : %c", head.value, appendedNode.prevVal.value)
 	}
+}
 
+func TestPrependNode(t *testing.T) {
+	head := createNode('A')
 	newHead, _ := prependNode(head, 'Z')
 	if newHead == nil || newHead.value != 'Z' {
 		t.Errorf("error with the new head value, want : 'Z', got : %c", newHead.value)
@@ -25,5 +31,4 @@ func TestLinkedList(t *testing.T) {
 	if newHead.nextVal != head {
 		t.Errorf("error with the nex node of the new head. Want : %c, got : %c", head.value, newHead.nextVal.value)
 	}
-	//TODO implement next tests
 }
