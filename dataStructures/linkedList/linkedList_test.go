358585ea2eeee367ff96c1949b3c228588b517ce
Mehdi Serra <mehdi.serra57@gmail.com>
2023-10-29 13:52:49 +0100

    Add error handle to the linkedList_test.go

--- dataStructures/linkedList/linkedList_test.go
+++ dataStructures/linkedList/linkedList_test.go
@@ -13,16 +13,21 @@ func TestCreateNode(t *testing.T) {
 func TestAppendNode(t *testing.T) {
 	head := createNode('A')
 	//Test appendNode function
-	appendedNode, _ := appendNode(head, 'B')
-	if appendedNode.value != 'B' {
+	appendedNode, errB := appendNode(head, 'B')
+
+	if errB != nil {
+		t.Errorf("appendNode(head, 'B') return an error. %v", errB)
+	} else if appendedNode.value != 'B' {
 		t.Errorf("error in appended node value, want 'B', got : %c", appendedNode.value)
-	}
-	if appendedNode.prevVal != head {
+	} else if appendedNode.prevVal != head {
 		t.Errorf("error in appended node previous value, want %c, got : %c", head.value, appendedNode.prevVal.value)
 	}
 
-	thirdNode, _ := appendNode(head, 'C')
-	if thirdNode.value != 'C' {
+	thirdNode, errC := appendNode(head, 'C')
+
+	if errC != nil {
+		t.Errorf("appendNode(head, 'C') return an error. %v", errC)
+	} else if thirdNode.value != 'C' {
 		t.Errorf("error in appended node value, want 'C', got : %c", thirdNode.value)
 	}
 	if thirdNode.prevVal != appendedNode {
@@ -32,15 +37,20 @@ func TestAppendNode(t *testing.T) {
 
 func TestAppendNode_returnErr(t *testing.T) {
 	appendedNode, err := appendNode(nil, 'B')
-	if err == nil || appendedNode != nil {
+	if err == nil {
+		t.Errorf("appendNode(nil, 'B') should return an error. %v", err)
+	} else if appendedNode != nil {
 		t.Errorf("Call appendNode with nil head should throw an error.")
 	}
 }
 
 func TestPrependNode(t *testing.T) {
 	head := createNode('A')
-	newHead, _ := prependNode(head, 'Z')
-	if newHead == nil || newHead.value != 'Z' {
+	newHead, err := prependNode(head, 'Z')
+
+	if err != nil {
+		t.Errorf("prependNode(head, 'Z'), return an error. %v", err)
+	} else if newHead == nil || newHead.value != 'Z' {
 		t.Errorf("error with the new head value, want : 'Z', got : %c", newHead.value)
 	}
 	if newHead.nextVal != head {
@@ -50,19 +60,30 @@ func TestPrependNode(t *testing.T) {
 
 func TestInsertNode(t *testing.T) {
 	head := createNode('A')
-	secondNode, _ := appendNode(head, 'B')
-	thirdNode, _ := appendNode(head, 'C')
+	secondNode, errB := appendNode(head, 'B')
+	thirdNode, errC := appendNode(head, 'C')
 
-	insertedNode, _ := insertNode(secondNode, 'I')
-	if insertedNode.prevVal != secondNode || insertedNode.nextVal != thirdNode {
+	if errB != nil || errC != nil {
+		t.Errorf("One of the appendNode method return an error. %v | %v", errB, errC)
+	}
+
+	insertedNode, errInsert := insertNode(secondNode, 'I')
+
+	if errInsert != nil {
+		t.Errorf("insertNode(secondNode, 'I') return an error. %v", errInsert)
+	} else if insertedNode.prevVal != secondNode || insertedNode.nextVal != thirdNode {
 		t.Errorf("Error during the insert, next and/or prev val are wrong.")
 	}
 }
 
 func TestDeleteNode(t *testing.T) {
 	head := createNode('A')
-	secondNode, _ := appendNode(head, 'B')
-	thirdNode, _ := appendNode(head, 'C')
+	secondNode, errB := appendNode(head, 'B')
+	thirdNode, errC := appendNode(head, 'C')
+
+	if errB != nil || errC != nil {
+		t.Errorf("One of the appendNode method return an error. %v | %v", errB, errC)
+	}
 
 	deleteNode(thirdNode)
 	if secondNode.nextVal != nil || thirdNode.prevVal != nil || thirdNode.nextVal != nil {
@@ -72,13 +93,15 @@ func TestDeleteNode(t *testing.T) {
 
 func TestDeleteNodeAtTheBeginning(t *testing.T) {
 	head := createNode('A')
-	secondNode, _ := appendNode(head, 'B')
-	_, _ = appendNode(head, 'C')
+	secondNode, errB := appendNode(head, 'B')
+	_, errC := appendNode(head, 'C')
+
+	if errB != nil || errC != nil {
+		t.Errorf("One of the appendNode method return an error. %v | %v", errB, errC)
+	}
 
 	deleteNode(head)
 	if secondNode.prevVal != nil || head.prevVal != nil || head.nextVal != nil {
 		t.Errorf("Error during delete, the node is still in the linked list.")
 	}
 }
-
-//TODO: Add test cases with failure + get err rather than _ on existing tests
